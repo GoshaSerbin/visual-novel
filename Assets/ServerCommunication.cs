0fbb2a24fa8dc0df59ebeaac19821a9b9aeb2825
GoshaSerbin <gosha_serbin00@mail.ru>
2024-03-20 11:06:32 +0300

    Removed JsonSerializer, replaced it with Unity JsonUtility for classes

--- Assets/ServerCommunication.cs
+++ Assets/ServerCommunication.cs
@@ -1,4 +1,3 @@
-// using System.Text.Json;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -9,6 +8,17 @@ public class ServerCommunication : MonoBehaviour
     public string serverURL = "http://";
     public string messageToSend = "Hello, server!";
 
+    public class Message
+    {
+        public Message(string Role, string Text)
+        {
+            role = Role;
+            text = Text;
+        }
+        public string role;
+        public string text;
+    }
+
     public void SendRequestToServer()
     {
         StartCoroutine(SendMessage());
@@ -18,12 +28,26 @@ public class ServerCommunication : MonoBehaviour
     IEnumerator SendMessage()
     {
         WWWForm form = new WWWForm();
-        form.AddField("message", messageToSend);
-        Dictionary<string, string> dict = new();
-        // string json = JsonSerializer.Serialize(dict);
-        // Console.WriteLine(json);
-        // form.AddField("prompt", )
 
+        Message message1 = new Message("system", "text");
+        Message message2 = new Message("user", "text");
+
+        var messages = new List<Message>() { message1, message2 };
+
+        string jsonMessages = "{";
+        foreach (Message message in messages)
+        {
+            string jsonMessage = JsonUtility.ToJson(message);
+            jsonMessages += jsonMessage;
+        }
+        jsonMessages += "}";
+        // var messages = new List<Dictionary<string, string>>();
+        // // TODO: add class for {role, text} ?
+        // messages.Add(new Dictionary<string, string>() { { "role", "system" }, { "text", messageToSend } });
+
+        // string jsonMessages = JsonSerializer.Serialize(messages);
+        Debug.Log(jsonMessages);
+        form.AddField("message", jsonMessages);
         using (UnityWebRequest www = UnityWebRequest.Post(serverURL, form))
         {
             yield return www.SendWebRequest();
