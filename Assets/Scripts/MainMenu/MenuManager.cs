c5ead3e1c48732db0654f72be55bc4b4be422d7a
GoshaSerbin <gosha_serbin00@mail.ru>
2024-04-17 09:51:15 +0300

    implemented simple menu scene

---
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+
+    public void ContinueGame()
+    {
+
+    }
+
+    public void OpenSettings()
+    {
+
+    }
+}
