fe053209ac3ea5d14752f1009835c0e69899fb59
MiraiRatchet <mirairatchetx@gmail.com>
2024-04-24 17:40:23 +0300

    text changing on mouse enter

--- Assets/Scripts/Map/LocationScript.cs
+++ Assets/Scripts/Map/LocationScript.cs
@@ -9,11 +9,25 @@ public class LocationScript : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] string _sceneName;
-    [SerializeField] string[] _inventoryOnLoc;
-    [SerializeField] string[] _peopleOnLoc;
+    [SerializeField] string _inventoryOnLoc;
+    [SerializeField] string _peopleOnLoc;
+
+    [SerializeField] TextMeshProUGUI _peopleText;
+    [SerializeField] TextMeshProUGUI _inventoryText;
 
     private void OnMouseDown()
     {
         SceneManager.LoadScene(_sceneName);
+    }
+
+    private void OnMouseEnter()
+    {
+        _peopleText.text = _peopleOnLoc;
+        _inventoryText.text = _inventoryOnLoc;
+    }
+    private void OnMouseExit()
+    {
+        _peopleText.text = "";
+        _inventoryText.text = "";
     }
 }
