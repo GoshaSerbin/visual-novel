43d0cc8d831873cd48135ec8cd8ff76ff93b564b
MiraiRatchet <mirairatchetx@gmail.com>
2024-04-26 20:52:01 +0300

    Adding map

---
+++ Assets/Scripts/Map/LocationScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LocationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] string _sceneName;
+    [SerializeField] string _inventoryOnLoc;
+    [SerializeField] string _peopleOnLoc;
+
+    [SerializeField] TextMeshProUGUI _peopleText;
+    [SerializeField] TextMeshProUGUI _inventoryText;
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    private void OnMouseEnter()
+    {
+        _peopleText.text = _peopleOnLoc;
+        _inventoryText.text = _inventoryOnLoc;
+    }
+    private void OnMouseExit()
+    {
+        _peopleText.text = "";
+        _inventoryText.text = "";
+    }
+}
