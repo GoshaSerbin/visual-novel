1a76dbcda0ccfa31473c082bd1332aacb6143fd4
GoshaSerbin <gosha_serbin00@mail.ru>
2024-05-08 12:57:50 +0300

    added more post processing

--- Assets/Scripts/Utils/TextProcessor.cs
+++ Assets/Scripts/Utils/TextProcessor.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public static class TextProcessor
 {
+
+    public static int MaxLength = 400;
     public static string PostProccess(string aiAnswer)
     {
         var charsToRemove = new string[] { "\n", "\"" };
         foreach (var c in charsToRemove)
         {
             aiAnswer = aiAnswer.Replace(c, string.Empty);
+        }
+        aiAnswer = aiAnswer.Replace('\n', ' ');
+        while (aiAnswer.Length > 400)
+        {
+            Debug.LogWarning("To much content...Will reduce it");
+            int index = aiAnswer.LastIndexOf('.');
+            if (index >= 0)
+            {
+                aiAnswer = aiAnswer.Substring(0, index + 1);
+            }
+            else
+            {
+                aiAnswer = aiAnswer.Substring(0, MaxLength) + "...";
+                break;
+            }
         }
         return aiAnswer;
     }
